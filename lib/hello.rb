28f80055cfe10349a7e1c6b4a8149d3e84e5d02a
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 17:35:36 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,6 @@
 def hello_t
-  name = ["Tim", "Tammy", "Fred"]
-  ["Tim", "Tammy", "Fred"].each do |name|
+  name = ["Tim", "Tom", "Fred"]
+  ["Tim", "Tom", "Fred"].each do |name|
     if name.start_with?("T")
       puts "Hi, #{name}"
     end
